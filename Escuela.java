53a1bac8f9ef62fc0f5253b9ee44594bdc9c90df
Saesar2317 <saesar.vazqueza@gmail.com>
2025-03-12 13:00:21 -0700

    Create Escuela.java

---
+++ Escuela.java
@@ -0,0 +1,12 @@
+package Alumnos;
+
+public class Escuela 
+{
+	public static void Datos(Alumno[] Alu) 
+	{
+		String [] Nombre= {"Calos", "Vane", "Paco", "PesoPluma", "PesoLapiz", "Baanin", "Andrea", "Papu", "Mia", "Littlecuh", "Ben10", "Leli", "Beliquin", "Tito", "Memo", "alexa", "Vini", "BarcoBasurero", "Vegeta", "Rayo"}
+		char [] Genero = {'M','M','M','M','M','F','F','F','F','F','M','M','F','F','F','M','M','M','F','F'}
+		
+	}
+
+}
